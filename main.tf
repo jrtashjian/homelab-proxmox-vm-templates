f6a1d8750fd04d6985a73549e4a3b8edf10d1a4e
JR Tashjian <hello@jrtashjian.com>
2024-01-27 22:10:19 -0500

    Update Debian cloud image source and checksum

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ resource "proxmox_virtual_environment_file" "debian_cloud_image" {
 
   source_file {
     # Obtain with: `shasum -a256 debian-12-genericcloud-amd64.qcow2`
-    checksum  = "16e360b50572092ff5c1ed994285bcca961df28c081b7bb5d7c006d35bce4914"
-    path      = "http://cloud.debian.org/images/cloud/bookworm/latest/debian-12-genericcloud-amd64.qcow2"
+    checksum  = "ef9270aee834900d5195b257d7580dc96483a298bf54e5c0555385dc23036e90"
+    path      = "https://cloud.debian.org/images/cloud/bookworm/20240102-1614/debian-12-genericcloud-amd64-20240102-1614.qcow2"
     file_name = "debian-12-genericcloud-amd64.img"
   }
 }
